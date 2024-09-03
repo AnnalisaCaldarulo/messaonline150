ba1896aa5193d9cacd2dc99301741b39bda903e7
Annalisa Caldarulo <annalisa.caldarulo@aulab.it>
2024-09-03 16:44:14 +0200

    margin bottom card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
             <h2 class="text-white text-center py-5">Tutti gli articoli</h2>
             @foreach ($articles as $article)
                 <div class="col-12 col-md-4 d-flex justify-content-center">
-                    <div class="card" style="width: 18rem;">
+                    <div class="card mb-3" style="width: 18rem;">
                         <div class="card-body">
                             <h5 class="card-title"> {{ $article->title }} </h5>
                             <h6 class="card-subtitle"> {{ $article->subtitle }} </h6>
